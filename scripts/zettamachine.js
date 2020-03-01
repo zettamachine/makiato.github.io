adccb5509f3301d995ef16e52edb204c54ffca02
Makiato von Zetta <mehmet.akyuz@soa-tr.com>
2020-03-01 11:16:49 +0000

    Updates

--- scripts/zettamachine.js
+++ scripts/zettamachine.js
@@ -1,2 +1,12 @@
+//contact 
 var user = 'hello', domain = 'zettamachine.com';
 document.getElementById('email').innerHTML = '<i class="fa fa-envelope"></i> <a href="mailto:' + user + '@' + domain + '">' + user + '@' + domain + '</a>';
+
+//nav background
+window.onscroll= function(e) {
+    if(window.scrollY > 0) {
+        document.querySelector('.navbar').style.backgroundColor = 'rgb(16,20,23)';
+    } else {
+        document.querySelector('.navbar').style.backgroundColor = 'rgba(16,20,23, 0.5)';
+    }
+}
